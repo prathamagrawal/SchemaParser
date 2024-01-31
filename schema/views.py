e1778ccb4f90bb67a56b640dd34b2b64e739c613
prathamagrawal <prathamagrawal1205@gmail.com>
2024-01-31 16:49:44 +0530

    Add: Success Page and Url mapping

--- schema/views.py
+++ schema/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SchemaModelForm,PropertiesFormset
-from django.http import HttpResponse
 from .models.properties import PropertiesModel
-from .models.schema import SchemaModel
 
 def create_schema_model(request):
     if request.method == 'GET':
@@ -13,15 +11,15 @@ def create_schema_model(request):
         formset = PropertiesFormset(request.POST)
         schemaform = SchemaModelForm(request.POST)
         if schemaform.is_valid() and formset.is_valid():
-            book = schemaform.save()
-            for form in formset:
-                property = form.save(commit=False)
-                property.schemaDetails = book
-                property.save()
-            return HttpResponse("Success")
-        
+            formset_instance=formset.save(commit=False)
+            schemaData = schemaform.save()
+            for form in formset_instance:
+                form.schemaDetails = schemaData
+                form.save()
+            return render(request, 'success.html',{'schemaData':schemaData,'propertyData':formset_instance})
 
     return render(request, 'create_schema.html', {
         'schemaform': schemaform,
         'formset': formset,
     })
+
